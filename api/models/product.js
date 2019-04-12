402204427c0970ea9b59ebd06ee85124debf7d79
JeeG2 <jiteshgupta499@gmail.com>
2019-04-12 15:12:31 +0530

    third commit

--- api/models/product.js
+++ api/models/product.js
@@ -6,7 +6,7 @@ const productSchema = mongoose.Schema({
     price: {type : Number, required : true},
     size: {type : Number, required : true},
     color: {type: String, required: true},
-    grade: {type: {'A':1,'B':2,'C':3}, required: true},
+    grade: {type: {'1':'A', '2':'B', '3':'C'}, required: true},
     created_at: {type: Date, required: true}
 });
 
--- api/routes/products.js
+++ api/routes/products.js
@@ -6,6 +6,35 @@ const Product = require('../models/product');
 const checkAuth = require('../authentication/check-auth');
 
 router.get('/', (req, res, next) => {
+    const qp = req.query;
+    if(qp)
+    {
+    Product.find(qp).select('name price color size grade created_at _id')
+    .exec()
+    .then(doc => {
+        console.log("From database",doc);
+        if(doc.length > 0)
+        {
+            res.status(200).json({
+                product : doc,
+                request : {
+                    type : 'GET',
+                    url : 'http://localhost:3000/products/'
+                }
+            });
+        }
+        else{
+            res.status(404).json({message : 'No valid entry found in the database'});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error : err});
+    });
+    }
+    else
+    {
+
     Product.find()
     .select('name price size color grade created_at _id')
     .exec()
@@ -43,9 +72,36 @@ router.get('/', (req, res, next) => {
         res.status(500).json({
             error : err
         });
-    });
+    });}
 });
 
+
+/*router.get('/', (req, res, next) => {
+    const qp = req.query;
+    Product.find(qp).select('name price color size grade created_at _id')
+    .exec()
+    .then(doc => {
+        console.log("From database",doc);
+        if(doc.length > 0)
+        {
+            res.status(200).json({
+                product : doc,
+                request : {
+                    type : 'GET',
+                    url : 'http://localhost:3000/products/'
+                }
+            });
+        }
+        else{
+            res.status(404).json({message : 'No valid entry found in the database'});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error : err});
+    });
+});
+*/
 router.post('/', checkAuth, (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
@@ -56,6 +112,9 @@ router.post('/', checkAuth, (req, res, next) => {
         grade: req.body.grade,
         created_at: new Date()
     });
+    if(product.name!=' ' && product.name!=='' && product.price!=' ' && product.price!==null && product.size!=' ' && product.size!==null && product.color!=' ' && product.color!=='' && product.grade!=' ' && product.grade!=='')
+    {
+    if(product.name.length<10 && (product.grade=='A' || product.grade=='B' || product.grade=='C')) {
     product.save().then(result => {
         console.log(result);
         res.status(201).json({
@@ -80,11 +139,23 @@ router.post('/', checkAuth, (req, res, next) => {
         res.status(500).json({
             error: err
         });
-    });
+    });}
+    else{
+        res.status(401).json({
+            message: "Your entries are wrong!"
+        });
+    }}
+    else{
+        res.status(401).json({
+            message:'Your entries are wrong!'
+        });
+    }
 });
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
+    if(mongoose.Types.ObjectId.isValid(id))
+    {
     Product.findById(id).select('name price size color grade created_at _id').exec().then(doc => {
         console.log("From database",doc);
         if(doc)
@@ -103,11 +174,21 @@ router.get('/:productId', (req, res, next) => {
     }).catch(err => {
         console.log(err);
         res.status(500).json({error : err});
-    });
+    });}
+    else{
+        res.status(401).json({
+            message:'ID is wrong!'
+        });
+    }
 });
 
 router.patch('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
+    if(mongoose.Types.ObjectId.isValid(id))
+    {
+    Product.findById(id).select('_id').exec().then(doc => {
+    if(doc)
+    {
     const updateOps = {};
     for(const ops of req.body)
     {
@@ -129,18 +210,33 @@ router.patch('/:productId', checkAuth, (req, res, next) => {
             error : err
         });
     });
-});
+    }
+    else{
+        res.status(404).json({message : 'No valid entry found in the database'});
+    }
+})
+}
+else{
+    res.status(401).json({
+        message:'The product id is INVALID!'
+    });
+}});
 
 router.delete('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
-    Product.remove({_id : id}).exec()
+    if(mongoose.Types.ObjectId.isValid(id))
+    {
+    Product.findById(id).select('_id').exec().then(doc => {
+    if(doc)
+    {
+    Product.deleteOne({_id:id}).exec()
     .then(result => {
         res.status(200).json({
             message : 'Product deleted',
             request : {
                 type : 'POST',
                 url : 'http://localhost:3000/products',
-                body : {name : 'String', price : 'Number', size: 'Number', color: 'String', grade: {'A':1,'B':2,'C':3}, created_at: 'Date'}
+                body : {name : 'String', price : 'Number', size: 'Number', color: 'String', grade: {'A':1,'B':2,'C':3}}
             }
         });
     })
@@ -150,6 +246,16 @@ router.delete('/:productId', checkAuth, (req, res, next) => {
             error : err
         });
     });
-});
+    }
+    else{
+        res.status(404).json({message : 'No valid entry found in the database'});
+    }
+})
+}
+else{
+    res.status(401).json({
+        message:'The product id is INVALID!'
+    });
+}});
 
 module.exports = router;
